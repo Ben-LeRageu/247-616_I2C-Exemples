ef67ba359e922cf03cced8b9aa7fd5c3cfea27ed
Ben-LeRageu <2265325@cegeplimoilou.ca>
2024-10-21 05:59:55 +0000

    version 1 partie3

--- interfaceVL6180x.c
+++ interfaceVL6180x.c
@@ -1,15 +1,25 @@
-#include "main.h"
-#include "piloteI2C1.h"
+//#include "main.h"
+//#include "piloteI2C1.h"
 #include "interfaceVL6180x.h"
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <sys/ioctl.h>
+#include <linux/i2c-dev.h> //for IOCTL defs
+#include <fcntl.h>
 
 //pour des détails: https://www.st.com/resource/en/design_tip/dt0037-vl6180x-range-and-ambient-light-sensor-quick-setup-guide-stmicroelectronics.pdf
 
 
 //définitions privée
+#define UC unsigned char
+#define UI unsigned int
+#define I2C_FICHIER "/dev/i2c-1" // fichier Linux representant le BUS #2
+#define I2C_ADRESSE 0x29 // adresse du Device I2C MPU-9250 (motion tracking)
 typedef struct
 {
-	uint16_t adresse;
-	uint8_t valeur;
+	UI uiAdresse;
+	UC ucValeur;
 } INTERFACEVL6810X_MESSAGE;
 
 #define INTERFACEVL6180X_ADRESSE 0x29 
@@ -37,7 +47,7 @@ INTERFACEVL6810X_MESSAGE interfaceVL6810x_message[INTERFACEVL6180X_NOMBRE] =
 	{0x0014, 0x24},// *Configures interrupt on ‘New Sample Ready threshold event’
 	{0x0016, 0x00} //*change fresh out of set status to 0
 };
-
+int fdPortI2C;
 //fonctions privées
 //pas de fonctions privées
 
@@ -45,36 +55,50 @@ INTERFACEVL6810X_MESSAGE interfaceVL6810x_message[INTERFACEVL6180X_NOMBRE] =
 //pas de variables publiques
 
 //fonctions publiques
-int interfaceVL6180x_ecrit(uint16_t Registre, uint8_t Donnee)
+int interfaceVL6180x_ecrit(UI Registre, UC Donnee)
 {
-uint8_t message[3];
-	message[0] = (uint8_t)(Registre >> 8);
-	message[1] = (uint8_t)(Registre & 0xFF);
-	message[2] = Donnee;
-	if (piloteI2C1_ecritDesOctets(message, 3) < 0)
+UC ucMessage[3];
+	ucMessage[0] = (UC)(Registre >> 8);
+	ucMessage[1] = (UC)(Registre & 0xFF);
+	ucMessage[2] = Donnee;
+	
+	if(write(fdPortI2C, &ucMessage, 3) < 0)
 	{
-		printf("erreur: interfaceVL6180x_ecrit\n");
+		printf("erreur: Écriture I2C\n");
+		close(fdPortI2C);
 		return -1;
 	}
+	//if (piloteI2C1_ecritDesOctets(ucMessage, 3) < 0)
+	//{
+	//	printf("erreur: interfaceVL6180x_ecrit\n");
+	//	return -1;
+	//}
 	return 0;
 }
 
-int interfaceVL6180x_lit(uint16_t Registre, uint8_t *Donnee)
+int interfaceVL6180x_lit(UI Registre, UC *Donnee)
 {
-uint8_t Commande[2];
-	Commande[0] = (uint8_t)(Registre >> 8);
-	Commande[1] = (uint8_t)Registre;
-	if (piloteI2C1_litDesOctets(Commande, 2, Donnee, 1) < 0)
+UC Commande[2];
+	Commande[0] = (UC)(Registre >> 8);
+	Commande[1] = (UC)Registre;
+	if (read(fdPortI2C, &Donnee, 1) < 0)
 	{
-		printf("erreur: interfaceVL6180x_lit\n");
+		printf("erreur: Lecture I2C\n");
+		close(fdPortI2C);
 		return -1;
 	}
+	//printf("octets lus: %#04x\n", Donnee);
+	//if (piloteI2C1_litDesOctets(Commande, 2, Donnee, 1) < 0)
+	//{
+	//	printf("erreur: interfaceVL6180x_lit\n");
+	//	return -1;
+	//}
 	return 0;
 }
 
 int interfaceVL6180x_litUneDistance(float *Distance)
 {
-uint8_t valeur;
+UC valeur;
 	if (interfaceVL6180x_ecrit(0x18, 0x01) < 0)
 	{
 		printf("erreur: interfaceVL6180x_litUneDistance 1\n");
@@ -104,38 +128,57 @@ uint8_t valeur;
     return -1;
 	}
 	*Distance = (float)valeur /10.0;
+	close(fdPortI2C); /// Fermeture du 'file descriptor'
 	return 0;
 }
 
 int interfaceVL6810x_initialise(void)
 {
-uint8_t index;
-uint8_t valeur;
-	if (piloteI2C1_configureLAdresse(INTERFACEVL6180X_ADRESSE) < 0)
+UC index;
+UC valeur;
+	//if (piloteI2C1_configureLAdresse(INTERFACEVL6180X_ADRESSE) < 0)
+	//{
+		//printf("erreur: interfaceVL6810x_initialise 1\n");
+	//	return -1;
+	//}
+
+  //if (interfaceVL6180x_lit(0x16, &valeur) < 0)
+  //{
+  	//printf("erreur: interfaceVL6180x_initialise 2\n");
+    //return -1;
+  //}
+	//if (valeur != 1)
+	//{
+	//	printf("message interfaceVL6180x: le VL6180x va être reconfiguré\n");
+	//}
+	
+	  // file descriptor I2C
+
+	// Initialisation du port I2C, 
+	fdPortI2C = open(I2C_FICHIER, O_RDWR); // ouverture du 'fichier', création d'un 'file descriptor' vers le port I2C
+	if(fdPortI2C == -1)
 	{
-		printf("erreur: interfaceVL6810x_initialise 1\n");
+		printf("erreur: I2C initialisation step 1\n");
+		return -1;
+	}
+	if(ioctl(fdPortI2C, I2C_SLAVE_FORCE, I2C_ADRESSE) < 0)  // I2C_SLAVE_FORCE if it is already in use by a driver (i2cdetect : UU)
+	{
+		printf("erreur: I2C initialisation step 2\n");
+		close(fdPortI2C);
 		return -1;
 	}
 
-  if (interfaceVL6180x_lit(0x16, &valeur) < 0)
-  {
-  	printf("erreur: interfaceVL6180x_initialise 2\n");
-    return -1;
-  }
-	if (valeur != 1)
-	{
-		printf("message interfaceVL6180x: le VL6180x va être reconfiguré\n");
-	}
-
-	for (index = 0; index < INTERFACEVL6180X_NOMBRE; index++)
-	{
-		if (interfaceVL6180x_ecrit(interfaceVL6810x_message[index].adresse, 
-				interfaceVL6810x_message[index].valeur) < 0)
-		{
-			printf("erreur: interfaceVL6180x_initialise 3 - index %d\n", index);
-			return -1;
-		}
-	}
+	write(fdPortI2C, &interfaceVL6810x_message, INTERFACEVL6180X_NOMBRE) ;
+	//for (index = 0; index < INTERFACEVL6180X_NOMBRE; index++)
+	//{
+	//	WriteByte(interfaceVL6810x_message[index]);
+		//if (interfaceVL6180x_ecrit(interfaceVL6810x_message[index].uiAdresse, 
+		//		interfaceVL6810x_message[index].ucValeur) < 0)
+		//{
+		//	printf("erreur: interfaceVL6180x_initialise 3 - index %d\n", index);
+		//	return -1;
+		//}
+	//}
 	return 0;
 }
 
--- main.c
+++ main.c
@@ -4,49 +4,38 @@
 #include <sys/ioctl.h>
 #include <linux/i2c-dev.h> //for IOCTL defs
 #include <fcntl.h>
+#include "interfaceVL6180x.h"
 
-#define I2C_FICHIER "/dev/i2c-2" // fichier Linux representant le BUS #2
-#define I2C_ADRESSE 0x68 // adresse du Device I2C MPU-9250 (motion tracking)
+#define I2C_FICHIER "/dev/i2c-1" // fichier Linux representant le BUS #2
+#define I2C_ADRESSE 0x29 // adresse du Device I2C MPU-9250 (motion tracking)
 
 int main()
 {
-	int fdPortI2C;  // file descriptor I2C
-
-	// Initialisation du port I2C, 
-	fdPortI2C = open(I2C_FICHIER, O_RDWR); // ouverture du 'fichier', création d'un 'file descriptor' vers le port I2C
-	if(fdPortI2C == -1)
-	{
-		printf("erreur: I2C initialisation step 1\n");
-		return -1;
-	}
-	if(ioctl(fdPortI2C, I2C_SLAVE_FORCE, I2C_ADRESSE) < 0)  // I2C_SLAVE_FORCE if it is already in use by a driver (i2cdetect : UU)
-	{
-		printf("erreur: I2C initialisation step 2\n");
-		close(fdPortI2C);
-		return -1;
-	}
-	
+	float Distance ;
 	// Écriture et Lecture sur le port I2C
-	uint8_t Source = 0x75; // registre d'ID du chip I2C
+	uint8_t Source[2] = {0x00, 0x00}; // registre d'ID du chip I2C
 	uint8_t Destination;
 	uint8_t NombreDOctetsALire = 1;
-	uint8_t NombreDOctetsAEcrire = 1;
+	uint8_t NombreDOctetsAEcrire = 2;
 
+	interfaceVL6810x_initialise();
+	interfaceVL6180x_litUneDistance(&Distance);
+	printf("/n La distance est de: %e", Distance);
 
-	if(write(fdPortI2C, &Source, NombreDOctetsAEcrire) != NombreDOctetsAEcrire)
-	{
-		printf("erreur: Écriture I2C\n");
-		close(fdPortI2C);
-		return -1;
-	}
-	if (read(fdPortI2C, &Destination, NombreDOctetsALire) != NombreDOctetsALire)
-	{
-		printf("erreur: Lecture I2C\n");
-		close(fdPortI2C);
-		return -1;
-	}
-	printf("octets lus: %#04x\n", Destination);
+	//if(write(fdPortI2C, &Source, NombreDOctetsAEcrire) != NombreDOctetsAEcrire)
+	//{
+	//	printf("erreur: Écriture I2C\n");
+	//	close(fdPortI2C);
+	//	return -1;
+	//}
+	//if (read(fdPortI2C, &Destination, NombreDOctetsALire) != NombreDOctetsALire)
+	//{
+	//	printf("erreur: Lecture I2C\n");
+	//	close(fdPortI2C);
+	//	return -1;
+	//}
+	//printf("octets lus: %#04x\n", Destination);
 
-	close(fdPortI2C); /// Fermeture du 'file descriptor'
+	
 	return 0;
 }
